e208c3d862f5f1b55064e4cd91d7fd396372847f
gram <git@orsinium.dev>
2022-11-10 10:52:07 +0100

    use functools.cached_property

--- flake8_todos/_checker.py
+++ flake8_todos/_checker.py
@@ -1,13 +1,12 @@
 from __future__ import annotations
 
-from functools import partial
+from functools import cached_property, partial
 from tokenize import TokenInfo, generate_tokens
 from typing import Iterable, Iterator
 
 import pycodestyle
 from flake8 import utils as stdin_utils
 
-from ._cached_property import cached_property
 from ._error import Error
 from ._rules import rules
 from ._token import Token
@@ -33,19 +32,19 @@ class Checker:
         self._tokens = file_tokens
 
     @cached_property
-    def lines(self) -> tuple[str]:
+    def lines(self) -> tuple[str, ...]:
         if self.filename == 'stdin':
             return stdin_utils.stdin_get_value().splitlines(True)
         return pycodestyle.readlines(self.filename)
 
     @cached_property
-    def tokens(self) -> tuple[Token, ...]:
+    def tokens(self) -> list[Token]:
         if self._tokens is not None:
             tokens = self._tokens
         else:
             getter = partial(next, iter(self.lines))
             tokens = generate_tokens(getter)  # type: ignore[arg-type]
-        return tuple(Token(token) for token in tokens)
+        return [Token(token) for token in tokens]
 
     def run(self) -> Iterator[tuple]:
         for error in self.get_errors():
